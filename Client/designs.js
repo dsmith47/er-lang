9d76b31b7c8565797712fa5886534c3800f3671a
Brynne DuBois <brynne@brynnes-mbp.dhcp.nd.edu>
2019-02-27 12:02:59 -0500

    Client (pixel canvas): done

---
+++ Client/designs.js
@@ -0,0 +1,22 @@
+// Designs.js
+
+function makeGrid(height, width){
+    $("tr").remove();
+    for (var i = 1; i <= height; i++) {
+        $("#pixelCanvas").append("<tr id=row" + i + "></tr>");
+        for (var y = 1; y <= width; y++) {
+            $("#row" + i).append("<td></td>");
+        }
+    }
+
+    $("td").click(function cellColor() {
+        color = $("#colorPicker").val();
+        if ($(this).attr("style")) {
+            $(this).removeAttr("style")
+        } else {
+            $(this).attr("style", "background-color: " + color);
+        }
+    });
+}
+
+makeGrid(40,40);
