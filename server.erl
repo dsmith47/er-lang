7cbedaf198b4f52f4feb9fbe32085150a4fb165e
Doug Smith <dsmith47@nd.edu>
2019-02-26 21:21:46 -0500

    GET/POST method discrimination

--- server.erl
+++ server.erl
@@ -3,8 +3,13 @@
 
 start() -> start(8080).
 
+% get_req_type(Str) -> lists:nth(1, string:split(lists:nth(1,
+%                                    strings:split(Str,"\n")), " ")).
+
+get_req_type(Str) -> lists:nth(1,vstrings:split(Str, "\n")).
+
 start(Port) ->
-    spawn(fun () -> {ok, Sock} = gen_tcp:listen(Port, [{active, false}]), 
+    spawn(fun () -> {ok, Sock} = gen_tcp:listen(Port, [list,{active, false},{packet,http}]), 
                     loop(Sock) end).
 
 loop(Sock) ->
@@ -13,9 +18,23 @@ loop(Sock) ->
     gen_tcp:controlling_process(Conn, Handler),
     loop(Sock).
 
+% TODO(dsmith47): this is no longer a binary, but a list
 handle(Conn) ->
+    %%gen_tcp:send(Conn, response("Hello World")),
+    case gen_tcp:recv(Conn, 0) of
+      {ok, {http_request, Method, Path, _Version}} ->
+                      case Method of
+                        'GET' -> gen_tcp:send(Conn,
+                                              response(get_canvas_response()));
+                        'POST' -> gen_tcp:send(Conn, response(post_pixel_response()))
+                      end
+    end,
     gen_tcp:send(Conn, response("Hello World")),
     gen_tcp:close(Conn).
+
+get_canvas_response() -> "GET".
+
+post_pixel_response() -> "POST".
 
 response(Str) ->
     B = iolist_to_binary(Str),
